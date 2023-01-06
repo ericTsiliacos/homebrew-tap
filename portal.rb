567c0fda9bfb74724160a5d570fd4da3a93fb769
ericTsiliacos <erictsiliacos@gmail.com>
2023-01-05 22:27:05 -0800

    Update portal.rb

--- portal.rb
+++ portal.rb
@@ -1,8 +1,8 @@
 class Portal < Formula
   desc "push and pull code between machines using git"
   homepage "https://github.com/ericTsiliacos/portal"
-  url "https://github.com/ericTsiliacos/portal/releases/download/v2.1.0/portal-v2.1.0-darwin-amd64.tar.gz"
-  sha256 "61e0ecd2c70efca382e73dd03e16da6f2c06a341dd6ce6b046bdd5ce73f80d25"
+  url "https://github.com/ericTsiliacos/portal/releases/download/v3.0.0/portal-v3.0.0-darwin-amd64.tar.gz"
+  sha256 "2f4a4d598d11e6ab557215fbf59f36dab4d27ef221b4bf4e29193384803b76c9"
 
   def install
     bin.install "portal"
